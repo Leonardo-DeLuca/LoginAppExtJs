d41a4a5448729ff877be3c83fef283608b91247e
leonardo.luca <leonardo.luca@useall.com.br>
2024-08-28 14:20:44 -0300

    salva user e password localstorage

--- app/view/login/LoginController.js
+++ app/view/login/LoginController.js
@@ -2,15 +2,34 @@ Ext.define('TutorialApp.view.login.LoginController', {
     extend: 'Ext.app.ViewController',
     alias: 'controller.login',
 
+    requires: [
+        'TutorialApp.view.login.LoginViewModel'
+    ],
+
+    init: function () {
+        var me = this;
+        me.getViewModel().set('username', localStorage.getItem('username'));
+        me.getViewModel().set('password', localStorage.getItem('password'));
+    },
+
     onLoginClick: function () {
+        var me = this,
+            form = me.getView().down('form'),
+            values = form.getValues();
 
-        localStorage.setItem("TutorialLoggedIn", true);
+        if (form.isValid()) {
+            localStorage.setItem("TutorialLoggedIn", true);
+            localStorage.setItem('username', values['{login.username}']);
+            localStorage.setItem('password', values.password);
+            // TODO: implement login logic here
+            console.log('Login successful!');
 
-        this.getView().destroy();
+            this.getView().destroy();
 
-        Ext.create({
-            xtype: 'app-main'
-        });
+            Ext.create({
+                xtype: 'app-main'
+            });
 
+        }
     }
 });
---
+++ app/view/login/LoginViewModel.js
@@ -0,0 +1,10 @@
+Ext.define('TutorialApp.view.login.LoginViewModel', {
+    extend: 'Ext.app.ViewModel',
+    alias: 'viewmodel.login',
+
+    data: {
+        username: '',
+        password: ''
+    },
+
+});
